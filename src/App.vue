887cb77ec7681d6117cdfc8f706b5337e57514b3
TvoyKot <a.lshe@yandex.by>
2024-12-24 18:34:51 +0300

    Fittin project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppHeader from './components/AppHeader.vue'
+</script>
+
+<template>
+  <div class="container">
+    <app-header />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+</template>
